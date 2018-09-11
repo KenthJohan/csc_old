889c2941c6a0ee9e994a9756e5ba74fa88d31a3d
KenthJohan <kenth.johan.work@gmail.com>
2018-09-11 17:01:09 +0200

    added2

--- gui.h
+++ gui.h
@@ -12,6 +12,40 @@ struct GUI_Vertex
 };
 
 
+void gui_vertex_print (struct GUI_Vertex * v, size_t n)
+{
+	for (size_t i = 0; i < 8; ++ i)
+	{
+		printf ("%f %f : %f %f %f %f\n", v [i].p [0], v [i].p [1], v [i].c [0], v [i].c [1], v [i].c [2], v [i].c [3]);
+	}
+}
+
+
+__attribute__((packed))
+struct GUI_VertexRectangle
+{
+	struct GUI_Vertex sw;
+	struct GUI_Vertex nw;
+	struct GUI_Vertex ne;
+	struct GUI_Vertex se;
+};
+
+
+void gui_vertex_layout 
+(GLuint vao, GLuint vbo, GLuint ebo, size_t v8, size_t e8, void * v, void * e)
+{
+	glBindVertexArray (vao);
+	glBindBuffer (GL_ARRAY_BUFFER, vbo);
+	glBufferData (GL_ARRAY_BUFFER, v8, v, GL_STATIC_DRAW);
+	glBindBuffer (GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData (GL_ELEMENT_ARRAY_BUFFER, e8, e, GL_STATIC_DRAW);
+	glVertexAttribPointer (0, 2, GL_FLOAT, GL_FALSE, sizeof (struct GUI_Vertex), (void*)0);
+	glEnableVertexAttribArray (0);
+	glVertexAttribPointer (1, 4, GL_FLOAT, GL_FALSE, sizeof (struct GUI_Vertex), (void*) (2 * sizeof (float)));
+	glEnableVertexAttribArray (1);
+}
+
+
 __attribute__((packed))
 struct GUI_RectangleSWNE
 {
@@ -21,7 +55,7 @@ struct GUI_RectangleSWNE
 
 
 __attribute__((packed))
-struct GUI_RectangleElements
+struct GUI_ElementsRectangle
 {
 	unsigned int v [6];
 };
@@ -43,7 +77,7 @@ void GUI_RectangleSWNE_NW
 }
 
 
-void gui_create_elements_from_rectangle (struct GUI_RectangleElements e [], size_t n)
+void gui_gen_elements (struct GUI_ElementsRectangle e [], size_t n)
 {
 	for (size_t i = 0; i < n; ++ i)
 	{
@@ -57,48 +91,136 @@ void gui_create_elements_from_rectangle (struct GUI_RectangleElements e [], size
 }
 
 
-void gui_create_vertex_from_rectangle 
-(struct GUI_Vertex v [], size_t vn, struct GUI_RectangleSWNE const x [], size_t xn)
+void gui_gen_vertices 
+(struct GUI_VertexRectangle v [], struct GUI_RectangleSWNE const x [], size_t xn)
 {
 	srand (0);
-	size_t j = 0;
 	for (size_t i = 0; i < xn; ++ i)
 	{
-		vf32_cpy (v [j].p, x [i].sw.v, 2);
-		vf32_random (v [j].c, 3);
-		v [j].c [3] = 1.0f;
-		j ++;
+		vf32_cpy (v [i].sw.p, x [i].sw.v, 2);
+		vf32_random (v [i].sw.c, 3);
+		v [i].sw.c [3] = 1.0f;
 		
-		GUI_RectangleSWNE_NW ((struct v2f32_xy *)v [j].p, x + i);
-		vf32_random (v [j].c, 3);
-		v [j].c [3] = 1.0f;
-		j ++;
+		GUI_RectangleSWNE_NW ((struct v2f32_xy *)v [i].nw.p, x + i);
+		vf32_random (v [i].nw.c, 3);
+		v [i].nw.c [3] = 1.0f;
 		
-		vf32_cpy (v [j].p, x [i].ne.v, 2);
-		vf32_random (v [j].c, 3);
-		v [j].c [3] = 1.0f;
-		j ++;
+		vf32_cpy (v [i].ne.p, x [i].ne.v, 2);
+		vf32_random (v [i].ne.c, 3);
+		v [i].ne.c [3] = 1.0f;
 		
-		GUI_RectangleSWNE_SE ((struct v2f32_xy *)v [j].p, x + i);
-		vf32_random (v [j].c, 3);
-		v [j].c [3] = 1.0f;
-		j ++;
+		GUI_RectangleSWNE_SE ((struct v2f32_xy *)v [i].se.p, x + i);
+		vf32_random (v [i].se.c, 3);
+		v [i].se.c [3] = 1.0f;
+	}
+}
+
+
+#define GUI_COLOR1 1 << 0
+
+struct GUI_Object
+{
+	uint32_t flags;
+};
+
+
+struct GUI
+{
+	GLuint vao;
+	GLuint vbo;
+	GLuint ebo;
+	
+	struct GUI_VertexRectangle * vertices;
+	struct GUI_RectangleSWNE * rectangles;
+	struct GUI_Object * objects;
+	size_t n;
+};
+
+
+
+
+
+void gui_init (struct GUI * g, size_t n)
+{
+	size_t v8 = sizeof (struct GUI_VertexRectangle) * n;
+	size_t e8 = sizeof (struct GUI_ElementsRectangle) * n;
+	struct GUI_ElementsRectangle * elements;
+	g->n = n;
+	//calloc allocates and set memory data to zero.
+	g->rectangles = calloc (n, sizeof (struct GUI_RectangleSWNE));
+	g->vertices = calloc (n, sizeof (struct GUI_VertexRectangle));
+	g->objects = calloc (n, sizeof (struct GUI_Object));
+	elements = malloc (e8);
+	glGenVertexArrays (1, &g->vao);
+	glGenBuffers (1, &g->vbo);
+	glGenBuffers (1, &g->ebo);
+	gui_gen_elements (elements, n);
+	gui_vertex_layout (g->vao, g->vbo, g->ebo, v8, e8, NULL, elements);
+	free (elements);
+}
+
+
+void gui_sync (struct GUI * g)
+{
+	gui_gen_vertices (g->vertices, g->rectangles, g->n);
+	size_t v8 = sizeof (struct GUI_VertexRectangle) * g->n;
+	glBufferSubData (GL_ARRAY_BUFFER, 0, v8, g->vertices);
+}
+
+
+void gui_draw (struct GUI * g, GLint uniform_color)
+{
+	//Number of rectangles (g->n).
+	size_t const n = g->n;
+	//Vertex array object.
+	GLuint const vao = g->vao;
+	
+	struct GUI_Object const * o = g->objects;
+	
+	glBindVertexArray (vao);
+	glDrawElements (GL_TRIANGLES, n * 6, GL_UNSIGNED_INT, 0);
+	
+	for (size_t i = 0; i < n; ++ i)
+	{
+		if (o [i].flags & GUI_COLOR1)
+		{
+			glUniform4f (uniform_color, 1.0, 0.0, 0.0, 0.0);
+			size_t m = i * 6 * sizeof (unsigned int);
+			glDrawElements (GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)m);
+		}
+	}
+	glUniform4f (uniform_color, 0.0, 0.0, 0.0, 0.0);
+}
+
+
+void gui_intersect_point (struct GUI * g, float p [2], int32_t flags_add, int32_t flags_remove)
+{
+	//Number of rectangles (g->n).
+	size_t const n = g->n;
+	
+	//Rectangles ((SW, NE), (SW, NE)) <=> ((vec2, vec2), (vec2, vec2))
+	//struct v2f32_xy * x = (struct v2f32_xy *) g->rectangles;
+	struct GUI_RectangleSWNE * r = g->rectangles;
+	
+	//vf32_print (stdout, p, 2, "%f ");
+	
+	for (size_t i = 0; i < n; ++ i)
+	{
+		float d [2];
+		v2f32_sub (d, r [i].sw.v, p);
+		//vf32_print (stdout, d, 2, "%f ");
+		if ((d [0] > 0.0f) || (d [1] > 0.0f)) {continue;}
+		v2f32_sub (d, r [i].ne.v, p);
+		//vf32_print (stdout, d, 2, "%f ");
+		if ((d [0] < 0.0f) || (d [1] < 0.0f)) {continue;}
+		g->objects [i].flags |= flags_add;
+		g->objects [i].flags &= ~flags_remove;
+		//printf ("%x\n", g->objects [i].flags);
 	}
 }
 
 
 
-
-void gui_hit (SDL_Window * window, SDL_MouseButtonEvent * m, struct GUI_RectangleSWNE const r [], size_t n)
-{
-	float x [2];
-	ui_mouse (x, window, m);
-	size_t hn = 2*2*2;
-	float h [2*2*2];
-	vf32_sub2 (h, (float const *)r, x, 4, 2);
-	vf32_print (stdout, x, 2, "%f ");
-	vf32_print2 (stdout, h, 4, 2, "%f ");
-}
 
 
 
--- v2.h
+++ v2.h
@@ -8,31 +8,36 @@ struct v2f32_xy
 
 
 // ret a > b
-int v2f32_gt_all (float const * a, float const * b)
+int v2f32_gt_all (float const a [2], float const b [2])
 {
 	return vf32_gt_all (a, b, 2);
 }
 
 
 // ret a < b
-int v2f32_lt_all (float const * a, float const * b)
+int v2f32_lt_all (float const a [2], float const b [2])
 {
 	return vf32_lt_all (a, b, 2);
 }
 
 
 // r := a > b
-void v2f32_gt (float * r, float const * a, float const * b)
+void v2f32_gt (float r [2], float const a [2], float const b [2])
 {
 	vf32_gt (r, a, b, 2);
 }
 
 
 // r := a < b
-void v2f32_lt (float * r, float const * a, float const * b)
+void v2f32_lt (float r [2], float const a [2], float const b [2])
 {
 	vf32_lt (r, a, b, 2);
 }
 
 
+// r := a - b
+void v2f32_sub (float r [2], float const a [2], float const b [2])
+{
+	vf32_sub (r, a, b, 2);
+}
 

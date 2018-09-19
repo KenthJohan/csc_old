2aca5d6598383ebd47fe8f955f8761841c4a79bf
KenthJohan <kenth.johan.work@gmail.com>
2018-09-19 09:55:00 +0200

    added comments

--- bufv.h
+++ bufv.h
@@ -7,20 +7,32 @@
 
 #define BUFV(type, name) \
 { \
+	/*Data pointer*/ \
 	type * p; \
+	/*Capacaty*/ \
 	size_t c; \
+	/*Number of elements*/ \
 	size_t n; \
 } name
+
 
 #define BUFV_INIT(type, buf, cap) \
 buf.c = cap; \
 buf.n = 0; \
 buf.p = (type *) calloc (buf.c, sizeof (type))
 
+//Allocate a clear buffeer all elements is set to zero.
+#define BUFV_CALLOC(type, buf, cap) \
+buf.c = cap; \
+buf.n = 0; \
+buf.p = (type *) calloc (buf.c, sizeof (type))
+
+//Grab the next free empty to (x) then grow the buffer.
 #define BUFV_PUSH(buf, count, x) \
 if ((buf.n + count) <= buf.c) {x = buf.p + buf.n; buf.n += count;} \
 else {x = NULL;} \
 /*ASSERT ((buf.i + count) < buf.n);*/
+
 
 #define BUFV_FULL(buf) (buf.n >= buf.c)
 

d98897dac903c616b415d0cfdb2a84db8fd83996
KenthJohan <kenth.johan.work@gmail.com>
2018-12-06 11:01:07 +0100

    removed

--- gl_drawarray.h
+++ gl_drawarray.h
@@ -4,37 +4,3 @@
 #include "debug.h"
 
 
-struct ProDraw
-{
-	uint32_t n;
-	uint32_t * offset;
-	uint32_t * length;
-	uint32_t * capacity;
-	GLenum * primitive;
-};
-
-
-void pd_init (struct ProDraw * pd)
-{
-	pd->offset = calloc (pd->n, sizeof (uint32_t));
-	pd->length = calloc (pd->n, sizeof (uint32_t));
-	pd->capacity = calloc (pd->n, sizeof (uint32_t));
-	pd->primitive = calloc (pd->n, sizeof (GLenum));
-}
-
-
-uint32_t pd_cap (struct ProDraw * pd)
-{
-	if (pd->n == 0) {return 0;}
-	uint32_t last = pd->n - 1;
-	return pd->offset [last] + pd->capacity [last];
-}
-
-
-void pd_draw (struct ProDraw * pd, uint32_t i)
-{
-	ASSERT (pd->primitive + i);
-	ASSERT (pd->offset + i);
-	ASSERT (pd->length + i);
-	glDrawArrays (pd->primitive [i], (GLint)pd->offset [i], (GLsizei)pd->length [i]);
-}

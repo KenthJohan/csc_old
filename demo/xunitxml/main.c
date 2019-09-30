e6b2f8bd052748f82a60f245e19edccc411f9aa3
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2019-09-30 21:23:14 +0200

    added modified argparse

--- demo/xunitxml/main.c
+++ demo/xunitxml/main.c
@@ -14,9 +14,13 @@
 #include <csc_readmisc.h>
 
 
-#define TCASE_COUNT 11
-#define TCASE_START_MEMORY_SIZE 128
-#define TCASE_FILENAME_MAXLEN 128
+#include <argparse.h>
+
+
+#define CASEINFO_COUNT 11
+#define CASEINFO_START_MEMORY_SIZE 128
+#define CASEINFO_FILENAME_MAXLEN 128
+#define CASEINFO_FINDCMD "find ../ -name \"*.c\""
 
 #define THREAD_COUNT 2
 #define THREAD_PRIORITY 1
@@ -26,7 +30,13 @@
 
 
 
-void threads_create (pthread_t t [], unsigned n, void *(* func)(void *), struct lfds711_stack_state * ss, int policy, int priority)
+#define DESCRIPTION0 \
+"\nThis is test-runner. It will find test and then run them in a thread pool."
+#define DESCRIPTION1 \
+"\nNo additional description of the program is available in this version."
+
+
+void threads_create (pthread_t t [], size_t n, void *(* func)(void *), struct lfds711_stack_state * ss, int policy, int priority)
 {
 	pthread_attr_t a;
 	pthread_attr_init(&a);
@@ -50,6 +60,7 @@ void threads_join (pthread_t t [], size_t n)
 	}
 }
 
+
 struct caseinfo
 {
 	struct lfds711_stack_element se;
@@ -62,11 +73,11 @@ struct caseinfo
 
 void caseinfo_popen (struct caseinfo * item)
 {
-	int r;
 	FILE * fp = popen ("netstat", "r");
-	item->memory_size = TCASE_START_MEMORY_SIZE;
+	item->memory_size = CASEINFO_START_MEMORY_SIZE;
 	item->memory = csc_readmisc_realloc (fileno (fp), &item->memory_size);
-	r = pclose (fp);
+	int r = pclose (fp);
+	printf ("pclose %i\n", r);
 	assert (item->memory);
 }
 
@@ -78,13 +89,13 @@ void * caseinfo_runner (void * arg)
 	while (1)
 	{
 		struct lfds711_stack_element * se;
-		struct caseinfo * td;
+		struct caseinfo * cinfo;
 		int r = lfds711_stack_pop (ss, &se);
-		sleep (rand() % 3);
 		if (r == 0) {break;}
-		td = LFDS711_STACK_GET_VALUE_FROM_ELEMENT (*se);
-		caseinfo_popen (td);
-		printf ("===========\nid %i. n %i. pclose %i\n%.*s\n", td->id, td->memory_size, r, 64, td->memory);
+		sleep (rand() % 2); //Sleep just for simulation
+		cinfo = LFDS711_STACK_GET_VALUE_FROM_ELEMENT (*se);
+		caseinfo_popen (cinfo);
+		printf ("===========\nid %i. n %i. pclose %i\n%.*s\n", cinfo->id, cinfo->memory_size, r, 64, cinfo->memory);
 	}
 	return NULL;
 }
@@ -92,15 +103,16 @@ void * caseinfo_runner (void * arg)
 
 void caseinfo_populate_filename (struct caseinfo items [], size_t n, char const * cmd)
 {
+	if (n == 0) {return;}
 	assert (items);
 	printf ("cmd %s\n", cmd);
 	FILE * fp = popen (cmd, "r");
 	assert (fp);
 	while (n--)
 	{
-		items [n].filename = malloc (TCASE_FILENAME_MAXLEN);
+		items [n].filename = malloc (CASEINFO_FILENAME_MAXLEN);
 		assert (items [n].filename);
-		char * r = fgets (items [n].filename, TCASE_FILENAME_MAXLEN, fp);
+		char * r = fgets (items [n].filename, CASEINFO_FILENAME_MAXLEN, fp);
 		if (r == NULL) {break;}
 		fputs (items [n].filename, stdout);
 	}
@@ -121,17 +133,60 @@ void caseinfo_populate_ss (struct caseinfo items [], size_t n, struct lfds711_st
 	}
 }
 
-
-int main (int argc, char * argv [])
+static const char *const usage[] =
 {
+"xunitxml [options] [[--] args]",
+"xunitxml [options]",
+NULL,
+};
+
+int main (int argc, char const * argv [])
+{
+	setbuf (stdout, NULL);
+	int thread_count = THREAD_COUNT;
+	int thread_policy = THREAD_POLICY;
+	int thread_priority = THREAD_PRIORITY;
+	int caseinfo_count = CASEINFO_COUNT;
+	const char *findcmd = NULL;
+	struct argparse_option options[] =
+	{
+		OPT_HELP(),
+		OPT_GROUP("Basic options"),
+		OPT_INTEGER('j', "thread_count", &thread_count, "number of threads", NULL, 0, 0),
+		OPT_INTEGER('s', "thread_policy", &thread_policy, "policy of each thread", NULL, 0, 0),
+		OPT_INTEGER('n', "thread_priority", &thread_priority, "priority of each thread", NULL, 0, 0),
+		OPT_INTEGER('k', "caseinfo_count", &caseinfo_count, "Maximum number of testcases", NULL, 0, 0),
+		OPT_STRING('p', "findcmd", &findcmd, "Use linux 'find' command here to find files to test", NULL, 0, 0),
+		OPT_END()
+	};
+	struct argparse argparse;
+	argparse_init(&argparse, options, usage, 0);
+	argparse_describe(&argparse, DESCRIPTION0, DESCRIPTION1);
+	argc = argparse_parse(&argparse, argc, argv);
+	printf ("thread_count: %d\n", thread_count);
+	printf ("thread_policy: %d\n", thread_policy);
+	printf ("thread_priority: %d\n", thread_priority);
+	printf ("caseinfo_count: %d\n", caseinfo_count);
+	if (findcmd == NULL) {findcmd = CASEINFO_FINDCMD;}
+	printf ("findcmd: %s\n", findcmd);
+	printf ("options [0].flags %x\n", options [0].flags);
+	if (options [0].flags & OPT_ENABLED)
+	{
+		exit (0);
+	}
+
+	assert (thread_count >= 0);
+	assert (caseinfo_count >= 0);
+	assert (thread_policy == SCHED_RR || thread_policy == SCHED_FIFO || thread_policy == SCHED_OTHER);
+
 	struct lfds711_stack_state ss;
 	lfds711_stack_init_valid_on_current_logical_core (&ss, NULL);
-	struct caseinfo * tcases = malloc (sizeof(struct caseinfo) * TCASE_COUNT);
-	caseinfo_populate_filename (tcases, TCASE_COUNT, "find ../ -name \"*.h\"");
-	caseinfo_populate_ss (tcases, TCASE_COUNT, &ss);
-	pthread_t threads [THREAD_COUNT];
-	threads_create (threads, THREAD_COUNT, caseinfo_runner, &ss, THREAD_POLICY, THREAD_PRIORITY);
-	threads_join (threads, THREAD_COUNT);
+	struct caseinfo * tcases = malloc (sizeof(struct caseinfo) * (size_t)caseinfo_count);
+	caseinfo_populate_filename (tcases, (size_t)caseinfo_count, findcmd);
+	caseinfo_populate_ss (tcases, (size_t)caseinfo_count, &ss);
+	pthread_t * threads = malloc (sizeof (pthread_t) * (size_t)thread_count);
+	threads_create (threads, (size_t)thread_count, caseinfo_runner, &ss, thread_policy, thread_priority);
+	threads_join (threads, (size_t)thread_count);
 	lfds711_stack_cleanup (&ss, NULL);
 	free (tcases);
 	return EXIT_SUCCESS;
--- demo/xunitxml/xunitxml.pro
+++ demo/xunitxml/xunitxml.pro
@@ -6,13 +6,15 @@ CONFIG -= qt
 INCLUDEPATH += $$PWD/../../src
 INCLUDEPATH += $$PWD/../../mxml-3.1
 INCLUDEPATH += $$PWD/../../liblfds711/inc
+INCLUDEPATH += $$PWD/../../argparse
 
 LIBS += -L$$PWD/../../mxml-3.1
 LIBS += -L$$PWD/../../liblfds711/bin
 LIBS += -lmxml -llfds711
 
-SOURCES += main.c xunit.c
+SOURCES += main.c xunit.c $$PWD/../../argparse/argparse.c
 
 HEADERS += $$PWD/xunit.h
 HEADERS += $$PWD/../../src/csc_readmisc.h
+HEADERS += $$PWD/../../argparse/argparse.h
 HEADERS += $$files($$PWD/../../liblfds711/inc/*.h, true)

3443078aff7460bfd7a3c6ab9a1a2bbb87d05724
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2019-09-30 13:16:57 +0200

    threadpool

--- demo/xunitxml/main.c
+++ demo/xunitxml/main.c
@@ -4,19 +4,16 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
+#include <unistd.h>
 
 #include <mxml.h>
 #include <liblfds711.h>
+#include <pthread.h>
 
 
-static void * worker (void *arg)
-{
-	/* Wait until tasks is available. */
-	//while (!queue_get(pool->pend_q, &t_ptr))
-	{
-		/* And then execute it. */
-	}
-}
+#define TEST_COUNT 100
+#define THREADS 3
+
 
 
 
@@ -26,26 +23,49 @@ struct test_data
 	int long long unsigned user_id;
 };
 
+static void * worker (void *arg)
+{
+	struct lfds711_stack_state * ss = arg;
+	while (1)
+	{
+		struct lfds711_stack_element * se;
+		struct test_data * td;
+		int r = lfds711_stack_pop (ss, &se);
+		sleep (rand() % 5);
+		if (r == 0) {break;}
+		td = LFDS711_STACK_GET_VALUE_FROM_ELEMENT (*se);
+		printf ("user_id = %llu\n", td->user_id);
+	}
+}
+
 int main (int argc, char * argv [])
 {
-	int long long unsigned loop;
-	struct lfds711_stack_element *se;
 	struct lfds711_stack_state ss;
-	struct test_data *td, *temp_td;
-	lfds711_stack_init_valid_on_current_logical_core( &ss, NULL );
-	td = malloc( sizeof(struct test_data) * 10 );
-	for( loop = 0 ; loop < 10 ; loop++ )
+	struct test_data *td;
+	lfds711_stack_init_valid_on_current_logical_core (&ss, NULL);
+	td = malloc( sizeof(struct test_data) * TEST_COUNT );
+	for (int long long unsigned i = 0; i < TEST_COUNT; ++i)
 	{
-		td[loop].user_id = loop;
-		LFDS711_STACK_SET_VALUE_IN_ELEMENT (td[loop].se, &td[loop]);
-		lfds711_stack_push (&ss, &td[loop].se);
+		td[i].user_id = i;
+		LFDS711_STACK_SET_VALUE_IN_ELEMENT (td[i].se, &td[i]);
+		lfds711_stack_push (&ss, &td[i].se);
 	}
 
-	for( loop = 0 ; loop < 10 ; loop++ )
+	pthread_t threads[THREADS];
+	for (int i = 0; i < THREADS; i++)
 	{
-		lfds711_stack_pop( &ss, &se );
-		temp_td = LFDS711_STACK_GET_VALUE_FROM_ELEMENT (*se);
-		printf ("user_id = %llu\n", temp_td->user_id);
+		pthread_create (&threads[i], NULL, worker, &ss);
+	}
+	for (int i = 0; i < THREADS; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+
+	//for( loop = 0 ; loop < 10 ; loop++ )
+	{
+		//lfds711_stack_pop (&ss, &se);
+		//temp_td = LFDS711_STACK_GET_VALUE_FROM_ELEMENT (*se);
+		//printf ("user_id = %llu\n", temp_td->user_id);
 	}
 
 	lfds711_stack_cleanup (&ss, NULL);
